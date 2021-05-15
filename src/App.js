c8d205b58ffa667b74fa000a1fd6fe9f3bfc4902
allexib <ooomegasib@gmail.com>
2021-05-15 21:09:06 +0400

    useEffect One more start

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function App() {
+
+  const  [numbers, setNumbers] = React.useState([1, 555, 3]);
+
+  const addNumber = () => {
+    const randNumber = Math.round(Math.random() * 10);
+    const newArr = [...numbers, randNumber];
+    setNumbers(newArr);
+  };
+  return (
+  <div className="App">
+    <ul>
+        {numbers.map((num, index) => (
+            <li key={index}>{num}</li>
+        ))}
+    </ul>
+    <button onClick={addNumber}>Новое число</button>
+  </div>
+  );
+}
+
+export default App;
