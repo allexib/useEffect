1ee4f742f4773857e7f4b95e9772d4cb675a1ed5
allexib <ooomegasib@gmail.com>
2021-05-16 11:18:15 +0400

    modification class component List, test

--- src/List.jsx
+++ src/List.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 
-const List = () => {
-    const [numbers, setNumbers] = React.useState([1, 555, 3]);
+class List extends React.Component {
 
-    const addNumber = () => {
-        const randNumber = Math.round(Math.random() * 10);
-        const newArr = [...numbers, randNumber];
-        setNumbers(newArr);
-    };
-
-    return (
-        <div>
-            <ul>
-                {numbers.map((num, index) => (
-                    <li key={index}>{num}</li>
-                ))}
-            </ul>
-            <button onClick={addNumber}>Новое число</button>
-        </div>
-    );
+    render() {
+        return (
+            <div>
+                <ul>
+                    test
+                    {/*{numbers.map((num, index) => (*/}
+                        {/*<li key={index}>{num}</li>*/}
+                    {/*))}*/}
+                </ul>
+                <button onClick={null}>Новое число</button>
+            </div>
+        );
+    }
 }
+
+// const List = () => {
+//     const [numbers, setNumbers] = React.useState([1, 555, 3]);
+//
+//     const addNumber = () => {
+//         const randNumber = Math.round(Math.random() * 10);
+//         const newArr = [...numbers, randNumber];
+//         setNumbers(newArr);
+//     };
+//
+//     return (
+//         <div>
+//             <ul>
+//                 {numbers.map((num, index) => (
+//                     <li key={index}>{num}</li>
+//                 ))}
+//             </ul>
+//             <button onClick={addNumber}>Новое число</button>
+//         </div>
+//     );
+// }
 export default List;
