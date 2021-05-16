ffcd9336052579688126feb7149310028a0e15d4
allexib <ooomegasib@gmail.com>
2021-05-16 12:49:45 +0400

    when componentDidMount

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import List from './List';
 
 function App() {
+    const [visibleList, setVisibleList] = React.useState(true);
 
+    const toggleVisibleList = () => {
+        setVisibleList(visible => !visible);
+    }
 
-  return (
-  <div className="App">
-    <List/>
-  </div>
-  );
+    return (
+        <div className="App">
+            {visibleList && <List/>}
+            <button onClick={toggleVisibleList}>show / hide</button>
+        </div>
+    );
 }
 
 export default App;
