1a2aa7c68d2d76a6c1244e754705fcbf65fc047b
allexib <ooomegasib@gmail.com>
2021-05-16 13:00:46 +0400

    fuctional componentDidMount

--- src/List.jsx
+++ src/List.jsx
@@ -1,65 +1,70 @@
 import React from "react";
 
-class List extends React.Component {
-    state = {
-        numbers: [1, 2, 3]
-    };
-
-    addRandonNumber = () => {
-        const randNumber = Math.round(Math.random() * 10);
-        this.setState({
-            numbers: [...this.state.numbers, randNumber]
-        });
-    };
-
-    componentDidMount() {
-        console.log('компонент был отображен');
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        //console.log({prevProps, prevState, nextProps: this.props, nextState: this.state});
-        if (this.state.numbers.length !== prevState.numbers.length) {
-            console.log('список чисел обновился');
-        }
-    }
-
-    componentWillUnmount() {
-        console.log('компонент будет удален');
-    }
-
-    render() {
-        return (
-            <div>
-                <ul>
-
-                    {this.state.numbers.map((num, index) => (
-                        <li key={index}>{num}</li>
-                    ))}
-                </ul>
-                <button onClick={this.addRandonNumber}>Новое число</button>
-            </div>
-        );
-    }
-}
-
-// const List = () => {
-//     const [numbers, setNumbers] = React.useState([1, 555, 3]);
-//
-//     const addNumber = () => {
-//         const randNumber = Math.round(Math.random() * 10);
-//         const newArr = [...numbers, randNumber];
-//         setNumbers(newArr);
+// class List extends React.Component {
+//     state = {
+//         numbers: [1, 2, 3]
 //     };
 //
-//     return (
-//         <div>
-//             <ul>
-//                 {numbers.map((num, index) => (
-//                     <li key={index}>{num}</li>
-//                 ))}
-//             </ul>
-//             <button onClick={addNumber}>Новое число</button>
-//         </div>
-//     );
+//     addRandonNumber = () => {
+//         const randNumber = Math.round(Math.random() * 10);
+//         this.setState({
+//             numbers: [...this.state.numbers, randNumber]
+//         });
+//     };
+//
+//     componentDidMount() {
+//         console.log('компонент был отображен');
+//     }
+//
+//     componentDidUpdate(prevProps, prevState) {
+//         //console.log({prevProps, prevState, nextProps: this.props, nextState: this.state});
+//         if (this.state.numbers.length !== prevState.numbers.length) {
+//             console.log('список чисел обновился');
+//         }
+//     }
+//
+//     componentWillUnmount() {
+//         console.log('компонент будет удален');
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <ul>
+//
+//                     {this.state.numbers.map((num, index) => (
+//                         <li key={index}>{num}</li>
+//                     ))}
+//                 </ul>
+//                 <button onClick={this.addRandonNumber}>Новое число</button>
+//             </div>
+//         );
+//     }
 // }
+
+const List = () => {
+    const [numbers, setNumbers] = React.useState([1, 555, 3]);
+
+    const addNumber = () => {
+        const randNumber = Math.round(Math.random() * 10);
+        const newArr = [...numbers, randNumber];
+        setNumbers(newArr);
+    };
+
+    //componentDidMount
+    React.useEffect(() => {
+        console.log('компонент был отображен');
+    }, []);
+
+    return (
+        <div>
+            <ul>
+                {numbers.map((num, index) => (
+                    <li key={index}>{num}</li>
+                ))}
+            </ul>
+            <button onClick={addNumber}>Новое число</button>
+        </div>
+    );
+}
 export default List;
